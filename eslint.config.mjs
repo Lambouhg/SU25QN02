896100dcdee74aba337335b12700067719a0a96b
Lambouhg <viett9961@gmail.com>
2025-08-22 16:39:36 +0700

    [RM_84] Unit-Test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "src/__tests__/**",
+      "vitest.setup.ts",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
